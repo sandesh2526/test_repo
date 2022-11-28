57dbbf18d721ae20343e5daba534a6e79983c89c
sandesh2526 <sandesh.parbhankar@gmail.com>
2022-11-28 20:04:17 +0530

    Added sample.java

---
+++ sample.java
@@ -0,0 +1,18 @@
+package test_repo;
+
+public class sample
+{
+    private int empId;
+    private String empName;
+    private String empDepartment;
+    private static int counter = 0;
+    sample(String name, String Department)
+    {
+        this.empId = counter;
+        counter++;
+    }
+    public static void main(String[] args)
+    {
+        sample s = new sample("Jim","Harper");
+    }
+}
