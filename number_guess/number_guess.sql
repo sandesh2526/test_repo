e84f3ceeecf0321f3defc1cad324183d3e659c62
sandesh2526 <56215314+sandesh2526@users.noreply.github.com>
2023-08-23 00:31:08 +0530

    Create number_guess.sql

---
+++ number_guess/number_guess.sql
@@ -0,0 +1,23 @@
+128);
+INSERT INTO public.userstats VALUES ('user_1692729719295', 5, 19);
+INSERT INTO public.userstats VALUES ('user_1692730024435', 2, 500);
+INSERT INTO public.userstats VALUES ('user_1692730024436', 5, 66);
+INSERT INTO public.userstats VALUES ('user_1692730221989', 2, 370);
+INSERT INTO public.userstats VALUES ('user_1692730221990', 5, 449);
+INSERT INTO public.userstats VALUES ('535', 8, 3);
+INSERT INTO public.userstats VALUES ('user_1692730306890', 2, 188);
+INSERT INTO public.userstats VALUES ('user_1692730306891', 5, 131);
+
+
+--
+-- Name: userstats userstats_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.userstats
+    ADD CONSTRAINT userstats_pkey PRIMARY KEY (username);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
